af9f26d23183f2493ce02a3e3d38d78330558f23
Abdelazim Ali <blackflower225@gmail.com>
2017-10-20 20:02:33 +0200

    some edit for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 5000;
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -9,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     let now = new Date().toString();
     let log = `${now}: ${req.method} ${req.url}`;
-    fs.appendFile('server.log',log + '\n',(err)=>{
+    fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('unable to save log');
         }
@@ -44,7 +45,13 @@ app.get('/bad', (req, res) => {
     res.send({
         errorMassegs: '404 Not Found'
     });
-})
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    else{
+        console.log(`Example app listening on port ${port}!`)        
+    }
 })
